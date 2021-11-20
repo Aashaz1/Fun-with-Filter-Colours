85df2e4b727bb5b38a7ceed31a17dc695eeffda7
Aashaz1 <88235121+Aashaz1@users.noreply.github.com>
2021-11-20 16:39:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function preload(){
+}
+
+function setup(){
+    canvas = createCanvas(640, 480);
+    canvas.position(110, 250);
+    video = createCapture(VIDEO);
+    video.hide();
+
+    tintColour = "";
+}
+
+function draw(){
+    image(video, 0, 0, 640, 480);
+    tint(tintColour);
+}
+
+function takeSnapshot(){
+    save('studentName.png');
+}
+
+function filterTint(){
+    tintColour = document.getElementById("colourName").value;
+}
